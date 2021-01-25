7e705bcd4b2397b6bedfa36f10ff35a73a00d57e
Thien <thienngu2002@gmail.com>
2021-01-25 09:41:57 +0100

    Stop................

--- mastermind/data/testing/unittest.py
+++ mastermind/data/testing/unittest.py
@@ -6,6 +6,7 @@ from mastermind.data.game_settings_data import GameSettings
 
 # built-in imports
 import unittest
+#from mastermind.data.game_settings_data import GameSettings
 
 class TestGameSettingsData(unittest.TestCase):
     """Test for the game_settigns_data.py"""
@@ -13,7 +14,7 @@ class TestGameSettingsData(unittest.TestCase):
         self.game_settings = GameSettings(':memory:')
 
     def test_add(self):
-        pass
+        self.assertEqual(GameSettings.add("joemama", ["#BF616A", "#D08770", "#EBCB8B", "#A3BE8C", "#88C0D0"], 4, 10, "00:00"), GameSettings.get_all(1))
 
     def test_remove(self):
         pass
