d936e3540152f15104693c07ab20c5c178ed9ea0
DaMalle <marcfriis@protonmail.com>
2021-02-23 11:03:50 -0800

    added auto-code generation to mastmind_logic

--- mastermind/logic/mastermind_logic.py
+++ mastermind/logic/mastermind_logic.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 
-class MastermindLogic:
+# Built-in imports
+from random import randint, shuffle
 
-    # Amount of correct guesses in colorcode
+class MastermindLogic:
     def check_correct_guess(self, colorCode, userCode):
         self.correctGuess = 0
         for i in range(len(colorCode)):
@@ -10,8 +11,6 @@ class MastermindLogic:
                 self.correctGuess += 1
         return self.correctGuess
 
-
-    # Amount of correct colors in colorcode
     def check_correct_color(self, colorCode, userCode):
         self.correctGuess = 0
         for i in range(len(colorCode)):
@@ -19,14 +18,20 @@ class MastermindLogic:
                 self.correctGuess += 1
         return self.correctGuess
 
-
     def check_win(self, colorCode, userCode):
         if userCode == colorCode:
             return True
         return False
 
-
     def check_gamestate(self, colorCode, userCode):
         self.correct_guesses = self.check_correct_guess(colorCode, userCode)
         self.correct_colors = self.check_correct_color(colorCode, userCode) - self.correct_guesses
         return (self.check_win(colorCode, userCode), self.correct_guesses, self.correct_colors)
+
+    def generate_code(self, colors):
+        return [colors[randint(0, len(colors)-1)] for _ in range(4)]
+
+    def generate_unique_code(self, colors):
+        if len(colors) >= 4:
+            shuffle(colors)
+            return colors[:4]
